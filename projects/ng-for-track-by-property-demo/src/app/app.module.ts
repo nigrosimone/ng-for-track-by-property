cd76986012fe259434a9db3db43e1b5733c36963
Nigro Simone <nigro.simone@gmail.com>
2024-08-05 09:36:30 +0200

    Angular 18

--- projects/ng-for-track-by-property-demo/src/app/app.module.ts
+++ projects/ng-for-track-by-property-demo/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { NgForTrackByPropertyModule } from '../../../ng-for-track-by-property/src/public-api';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    CommonModule,
-    HttpClientModule,
-    NgForTrackByPropertyModule,
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        CommonModule,
+        NgForTrackByPropertyModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
