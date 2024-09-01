4cc686cc9c2ac54f57c680fdf2946da69567dbf6
Nigro Simone <nigro.simone@gmail.com>
2024-09-01 20:58:46 +0200

    chore: better test

--- projects/ng-for-track-by-property/src/lib/ng-for-track-by-property.spec.ts
+++ projects/ng-for-track-by-property/src/lib/ng-for-track-by-property.spec.ts
@@ -1,107 +1,49 @@
-import { Component, DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { NgForTrackByPropertyModule } from './ng-for-track-by-property.module';
+import { CommonModule } from '@angular/common';
 
-@Component({ template: `<ul><li *ngFor="let item of list; trackByProperty: 'id'">{{ item.name }}</li></ul>` })
-class TestPropertyComponent {
-    list = [
-        { id: 0, name: 'foo' },
-        { id: 1, name: 'bar' },
-        { id: 2, name: 'baz' },
-    ];
-}
-describe('NgForTrackByProperty: trackByProperty', () => {
+describe('NgForTrackByProperty', () => {
 
-    let fixture: ComponentFixture<TestPropertyComponent>;
-    let debugElement: DebugElement;
-    let element: HTMLElement;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [TestPropertyComponent],
-            imports: [NgForTrackByPropertyModule]
-        });
-        fixture = TestBed.createComponent(TestPropertyComponent);
-        debugElement = fixture.debugElement;
-        element = debugElement.nativeElement;
-    });
-
-    afterEach(() => {
-        document.body.removeChild(element);
-    });
-
-    it('test', () => {
+    it('trackByProperty', () => {
+        @Component({ template: `<ul><li *ngFor="let item of list; trackByProperty: 'id'">{{ item.name }}</li></ul>`, standalone: true, imports: [CommonModule, NgForTrackByPropertyModule] })
+        class TestComponent {
+            list = [
+                { id: 0, name: 'foo' },
+                { id: 1, name: 'bar' },
+                { id: 2, name: 'baz' },
+            ];
+        }
+        const fixture = TestBed.createComponent(TestComponent);
         fixture.detectChanges();
-        expect(element.textContent).toBe('foobarbaz');
-    });
-});
-
-@Component({ template: `<ul><li *ngFor="let item of list; trackByIndex">{{ item.name }}</li></ul>` })
-class TestIndexComponent {
-    list = [
-        { id: 0, name: 'foo' },
-        { id: 1, name: 'bar' },
-        { id: 2, name: 'baz' },
-    ];
-}
-describe('NgForTrackByProperty: trackByIndex', () => {
-
-    let fixture: ComponentFixture<TestIndexComponent>;
-    let debugElement: DebugElement;
-    let element: HTMLElement;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [TestIndexComponent],
-            imports: [NgForTrackByPropertyModule]
-        });
-        fixture = TestBed.createComponent(TestIndexComponent);
-        debugElement = fixture.debugElement;
-        element = debugElement.nativeElement;
+        expect(fixture.nativeElement.textContent).toBe('foobarbaz');
     });
 
-    afterEach(() => {
-        document.body.removeChild(element);
-    });
-
-    it('test', () => {
+    it('trackByIndex', () => {
+        @Component({ template: `<ul><li *ngFor="let item of list; trackByIndex">{{ item.name }}</li></ul>`, standalone: true, imports: [CommonModule, NgForTrackByPropertyModule] })
+        class TestComponent {
+            list = [
+                { id: 0, name: 'foo' },
+                { id: 1, name: 'bar' },
+                { id: 2, name: 'baz' },
+            ];
+        }
+        const fixture = TestBed.createComponent(TestComponent);
         fixture.detectChanges();
-        expect(element.textContent).toBe('foobarbaz');
-    });
-});
-
-
-
-@Component({ template: `<ul><li *ngFor="let item of list; trackById">{{ item.name }}</li></ul>` })
-class TestIdComponent {
-    list = [
-        { id: 0, name: 'foo' },
-        { id: 1, name: 'bar' },
-        { id: 2, name: 'baz' },
-    ];
-}
-describe('NgForTrackByProperty: trackById', () => {
-
-    let fixture: ComponentFixture<TestIdComponent>;
-    let debugElement: DebugElement;
-    let element: HTMLElement;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [TestIdComponent],
-            imports: [NgForTrackByPropertyModule]
-        });
-        fixture = TestBed.createComponent(TestIdComponent);
-        debugElement = fixture.debugElement;
-        element = debugElement.nativeElement;
+        expect(fixture.nativeElement.textContent).toBe('foobarbaz');
     });
 
-    afterEach(() => {
-        document.body.removeChild(element);
-    });
-
-    it('test', () => {
+    it('trackById', () => {
+        @Component({ template: `<ul><li *ngFor="let item of list; trackById">{{ item.name }}</li></ul>`, standalone: true, imports: [CommonModule, NgForTrackByPropertyModule] })
+        class TestComponent {
+            list = [
+                { id: 0, name: 'foo' },
+                { id: 1, name: 'bar' },
+                { id: 2, name: 'baz' },
+            ];
+        }
+        const fixture = TestBed.createComponent(TestComponent);
         fixture.detectChanges();
-        expect(element.textContent).toBe('foobarbaz');
+        expect(fixture.nativeElement.textContent).toBe('foobarbaz');
     });
 });
