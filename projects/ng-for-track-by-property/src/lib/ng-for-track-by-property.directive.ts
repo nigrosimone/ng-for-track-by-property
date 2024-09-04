0cd9c5b8fb9d1564a3582aecf3c2237c40e793c2
Nigro Simone <nigro.simone@gmail.com>
2024-09-04 06:23:40 +0200

    chore: better docs

--- projects/ng-for-track-by-property/src/lib/ng-for-track-by-property.directive.ts
+++ projects/ng-for-track-by-property/src/lib/ng-for-track-by-property.directive.ts
@@ -1,39 +1,118 @@
 import { NgForOf } from '@angular/common';
-import { Directive, Host, Input, NgIterable } from '@angular/core';
+import { Directive, Host, Input, NgIterable, TrackByFunction } from '@angular/core';
 
+@Directive()
+export class NgForTrackByPropertyBaseDirective<T> {
+
+    @Input() ngForOf!: NgIterable<T>;
+
+    constructor(ngForOfDir: NgForOf<T>, fn: TrackByFunction<T>) {
+        ngForOfDir.ngForTrackBy = fn;
+    }
+}
+
+/**
+ * @ngModule NgForTrackByPropertyModule
+ *
+ * @description
+ *
+ * The `[ngForTrackByProperty]` directive provide an Angular trackBy.
+ *
+ * @usageNotes
+ *
+ * ### Usage
+ *
+ * ```html
+ * <ul>
+ *   <li *ngFor="let item of list; trackByProperty: 'id'">
+ *     id: {{ item.id }}; name: {{ item.name }}
+ *   </li>
+ * </ul>
+ * ```
+ */
 @Directive({
     selector: '[ngForTrackByProperty]'
 })
-export class NgForTrackByPropertyDirective<T> {
+export class NgForTrackByPropertyDirective<T> extends NgForTrackByPropertyBaseDirective<T> {
 
-    @Input() ngForOf!: NgIterable<T>;
+    /**
+     * @ngModule NgForTrackByPropertyModule
+     *
+     * @description
+     *
+     * The `[ngForTrackByProperty]` directive provide an Angular trackBy.
+     *
+     * @usageNotes
+     *
+     * ### Usage
+     *
+     * ```html
+     * <ul>
+     *   <li *ngFor="let item of list; trackByProperty: 'id'">
+     *     id: {{ item.id }}; name: {{ item.name }}
+     *   </li>
+     * </ul>
+     * ```
+     */
     @Input() ngForTrackByProperty!: keyof T;
 
     constructor(@Host() ngForOfDir: NgForOf<T>) {
-        ngForOfDir.ngForTrackBy = (_, item: T): T[keyof T] => item[this.ngForTrackByProperty];
+        super(ngForOfDir, (_, item: T): T[keyof T] => item[this.ngForTrackByProperty])
     }
 }
 
+/**
+ * @ngModule NgForTrackByPropertyModule
+ *
+ * @description
+ *
+ * The `[ngForTrackByIndex]` directive provide an Angular trackBy.
+ *
+ * @usageNotes
+ *
+ * ### Usage
+ *
+ * ```html
+ * <ul>
+ *   <li *ngFor="let item of list; ngForTrackByIndex">
+ *     id: {{ item.id }}; name: {{ item.name }}
+ *   </li>
+ * </ul>
+ * ```
+ */
 @Directive({
     selector: '[ngForTrackByIndex]'
 })
-export class NgForTrackByIndexDirective<T> {
-
-    @Input() ngForOf!: NgIterable<T>;
-
+export class NgForTrackByIndexDirective<T> extends NgForTrackByPropertyBaseDirective<T> {
     constructor(@Host() ngForOfDir: NgForOf<T>) {
-        ngForOfDir.ngForTrackBy = (index: number): number => index;
+        super(ngForOfDir, (index: number): number => index)
     }
 }
 
+/**
+ * @ngModule NgForTrackByPropertyModule
+ *
+ * @description
+ *
+ * The `[ngForTrackById]` directive provide an Angular trackBy.
+ *
+ * @usageNotes
+ *
+ * ### Usage
+ *
+ * ```html
+ * <ul>
+ *   <li *ngFor="let item of list; ngForTrackById">
+ *     id: {{ item.id }}; name: {{ item.name }}
+ *   </li>
+ * </ul>
+ * ```
+ */
 @Directive({
     selector: '[ngForTrackById]'
 })
-export class NgForTrackByIdDirective<T extends { id: number | string }> {
-
-    @Input() ngForOf!: NgIterable<T>;
-
+export class NgForTrackByIdDirective<T extends { id: number | string }> extends NgForTrackByPropertyBaseDirective<T> {
     constructor(@Host() ngForOfDir: NgForOf<T>) {
-        ngForOfDir.ngForTrackBy = (_, item: T) => item.id;
+        super(ngForOfDir, (_, item: T) => item.id)
     }
 }
