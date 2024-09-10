7257b319cd4c2cc5fab90d7ef96678300d4bf1bb
Nigro Simone <nigro.simone@gmail.com>
2024-09-10 19:25:59 +0200

    chore: format

--- projects/ng-for-track-by-property-demo/src/app/app.module.ts
+++ projects/ng-for-track-by-property-demo/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { NgForTrackByPropertyModule } from '../../../ng-for-track-by-property/src/public-api';
 
-@NgModule({ declarations: [AppComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
+@NgModule({
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
         FormsModule,
         CommonModule,
-        NgForTrackByPropertyModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        NgForTrackByPropertyModule
+    ]
+})
 export class AppModule { }
